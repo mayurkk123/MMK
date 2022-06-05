58ae5beae6d30f88895fae1b1f4967501e6c9f80
Admin <Admin@DESKTOP-5V6VI5K>
2022-06-05 22:26:20 +0530

    First Commit

---
+++ Kishore/src/Inheritance/Kes.java
@@ -0,0 +1,19 @@
+package Inheritance;
+
+public class Kes {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		String str ="mayur keskar";
+
+		int size= str.size();
+
+		for(int i =size; i=<13; i--)
+		{
+		System.out.println(str.length(i));
+		}
+
+	}
+
+}
---
+++ Kishore/src/Inheritance/child.java
@@ -0,0 +1,30 @@
+package Inheritance;
+
+class child {
+
+	public int addition(int a, int b) {
+		return a +b;
+	}
+	
+	public int substraction(int a, int b) {
+		return a-b;
+	}
+}
+	
+	public class Methodoverriding extends child{
+		public int addition(int a, int b) {
+			return a +b;
+		}
+		
+		
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Methodoverriding obj = new Methodoverriding();
+		obj.addition(10,20);
+	}
+
+	}
+	}
+	}
